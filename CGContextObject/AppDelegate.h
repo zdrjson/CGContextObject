b818f962fe6c1169400f022ae4944e3b17abbf78
YouXianMing <705786299@qq.com>
2015-07-02 22:04:21 +0800

    Initial Commit

---
+++ CGContextObject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CGContextObject
+//
+//  Created by YouXianMing on 15/7/2.
+//  Copyright (c) 2015年 YouXianMing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
